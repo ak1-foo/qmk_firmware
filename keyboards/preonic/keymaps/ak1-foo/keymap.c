984f7a961d5b77490ac68152903d7ab30cca2fd7
ak1-foo <74660186+ak1-foo@users.noreply.github.com>
2022-05-20 13:44:19 +0900

    remove one shot mod key

--- keyboards/preonic/keymaps/ak1-foo/keymap.c
+++ keyboards/preonic/keymaps/ak1-foo/keymap.c
@@ -9,15 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       XXXXXXX,         KC_Q,          KC_L,           KC_E,    KC_W,    KC_COMMA,  KC_B,  KC_M,    KC_Y,    KC_H,    KC_P,    XXXXXXX, \
       XXXXXXX,        KC_A,          KC_O,           KC_U,    KC_I,    KC_DOT,    KC_G,  KC_T,    KC_N,    KC_R,    KC_S,    XXXXXXX, \
       XXXXXXX,        KC_C,          KC_V,           KC_X,    KC_J,    KC_ESC,    KC_Z,  KC_K,    KC_F,    KC_D,    KC_MINUS, XXXXXXX, \
-      XXXXXXX,        LALT(KC_LEFT), KC_LALT,        OSM(MOD_LSFT), KC_SPACE,  LOWER, RAISE,   KC_ENT, OSM(MOD_LCTL), XXXXXXX,   LALT(KC_F10),QWERTY \
-      ),
-
-  [_QWERTY] = LAYOUT_preonic_grid( \
-      XXXXXXX, KC_1,     KC_2,     KC_3,    KC_4,    KC_5,       _______,     _______, _______, _______, _______, _______, \
-      KC_ESC,  KC_Q,      KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,        KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-      KC_TAB,  KC_A,     KC_S,    KC_D,    KC_F,    KC_G,       KC_H,        KC_LEFT,    KC_DOWN,    KC_UP,    KC_RIGHT, _______, \
-      KC_LSFT, KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,       KC_N,        KC_M,    KC_COMM, KC_DOT,  KC_SLSH, QED, \
-      KC_LCTL, _______,  KC_ENT,  KC_M,    KC_LALT, KC_SPC,     ADJUST,   KC_ENT,  KC_LGUI, KC_LCTL,   LALT(KC_F10),QWERTY \
+      XXXXXXX,        XXXXXXX, XXXXXXX,        KC_LSFT, KC_SPACE,  LOWER, RAISE,   KC_ENT, KC_LCTRL, XXXXXXX,   XXXXXXX, XXXXXXX \
       ),
 
   [_LOWER] = LAYOUT_preonic_grid( \
